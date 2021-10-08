f18bc76fc080ec1a4dd33a00d8cd183a7cb9a7d3
Igor Khomenko <webigorkiev@gmail.com>
2021-10-08 21:28:20 +0300

    add nessasery const

--- sse_neon.h
+++ sse_neon.h
@@ -12,14 +12,14 @@
     typedef int64x2_t __m128i; /* 128-bit vector containing integers */
     // Loads 128-bit value. :
     // https://msdn.microsoft.com/zh-cn/library/f4k12ae8(v=vs.90).aspx
-    FORCE_INLINE __m128i _mm_loadu_si128(const __m128i *p)
+    __m128i _mm_loadu_si128(const __m128i *p)
     {
         return vreinterpretq_m128i_s32(vld1q_s32((const int32_t *) p));
     }
 
     // Stores four 32-bit integer values as (as a __m128i value) at the address p.
     // https://msdn.microsoft.com/en-us/library/vstudio/edk11s13(v=vs.100).aspx
-    FORCE_INLINE void _mm_store_si128(__m128i *p, __m128i a)
+    void _mm_store_si128(__m128i *p, __m128i a)
     {
         vst1q_s32((int32_t *) p, vreinterpretq_s32_m128i(a));
     }
@@ -33,7 +33,7 @@
     //   r3 := a3 + b3
     //
     // https://msdn.microsoft.com/en-us/library/vstudio/09xs4fkk(v=vs.100).aspx
-    FORCE_INLINE __m128i _mm_add_epi32(__m128i a, __m128i b)
+    __m128i _mm_add_epi32(__m128i a, __m128i b)
     {
         return vreinterpretq_m128i_s32(
         vaddq_s32(vreinterpretq_s32_m128i(a), vreinterpretq_s32_m128i(b)));
@@ -42,7 +42,7 @@
     // Adds the 4 signed or unsigned 64-bit integers in a to the 4 signed or
     // unsigned 32-bit integers in b.
     // https://msdn.microsoft.com/en-us/library/vstudio/09xs4fkk(v=vs.100).aspx
-    FORCE_INLINE __m128i _mm_add_epi64(__m128i a, __m128i b)
+    __m128i _mm_add_epi64(__m128i a, __m128i b)
     {
         return vreinterpretq_m128i_s64(
         vaddq_s64(vreinterpretq_s64_m128i(a), vreinterpretq_s64_m128i(b)));
